70d017b2d09eeb577ecc035851ca0e07042886cd
The Errant One (Paliu)... <patezinhoverde@gmail.com>
2025-07-08 00:13:50 -0300

    Update and rename Main.bat to src/Main.bat

--- Main.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-:: shutdown /s /f /t 15 /c \"Your computer will shutdown in 15 seconds.\"
-echo teesteeeee
-pause
-exit
---
+++ src/Main.bat
@@ -0,0 +1,3 @@
+@echo off
+shutdown /s /f /t 15 /c \"Your computer will shutdown in 15 seconds.\"
+exit
